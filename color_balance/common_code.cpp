77f4f50e6eed8a4b5fa2eca9c0446b94e015a24c
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2024-10-19 20:17:55 +0200

    Completed color_balance

--- color_balance/common_code.cpp
+++ color_balance/common_code.cpp
@@ -9,7 +9,11 @@ cv::Mat fsiv_color_rescaling(const cv::Mat &in, const cv::Scalar &from, const cv
     // TODO
     // HINT: use cv:divide to compute the scaling factor.
     // HINT: use method cv::Mat::mul() to scale the input matrix.
+    cv::Scalar scale;
+    cv::divide(to, from, scale);
 
+    //out = in.mul(scale);
+    cv::multiply(in, scale, out);
     //
     CV_Assert(out.type() == in.type());
     CV_Assert(out.rows == in.rows && out.cols == in.cols);
@@ -22,7 +26,9 @@ cv::Mat fsiv_gray_world_color_balance(cv::Mat const &in)
     cv::Mat out;
     // TODO
     //  HINT: use cv::mean to compute the mean pixel value.
+    cv::Scalar scale = cv::mean(in);
 
+    out = fsiv_color_rescaling(in, scale, cv::Scalar(128, 128, 128));
     //
     CV_Assert(out.type() == in.type());
     CV_Assert(out.rows == in.rows && out.cols == in.cols);
@@ -34,7 +40,7 @@ cv::Mat fsiv_convert_bgr_to_gray(const cv::Mat &img, cv::Mat &out)
     CV_Assert(img.channels() == 3);
     // TODO
     // HINT: use cv::cvtColor()
-
+    cv::cvtColor(img, out, cv::COLOR_BGR2GRAY);
     //
     CV_Assert(out.channels() == 1);
     return out;
@@ -46,6 +52,13 @@ cv::Mat fsiv_compute_image_histogram(cv::Mat const &img)
     cv::Mat hist;
     // TODO
     // Hint: use cv::calcHist() and cv::normalize() to do the work.
+
+    std::vector<cv::Mat> in = {img};
+    std::vector<int> chs = {0};
+    std::vector<int> histSize = {256};
+    std::vector<float> ranges = {0, 256};
+
+    cv::calcHist(in, chs, cv::Mat(), hist, histSize, ranges, false);
 
     //
     CV_Assert(!hist.empty());
@@ -67,6 +80,14 @@ float fsiv_compute_histogram_percentile(cv::Mat const &hist, float p_value)
     //           sum(h[0], h[1], ... , h[p]) >= p_value*area(hist)
     // Hint: use cv::sum() to compute the histogram area.
 
+    float area = 0;
+    for (int i = 0; i < hist.rows; i++){
+        area += cv::sum(hist.at<float>(i))[0];
+        if (area >= p_value*cv::sum(hist)[0]){
+            p = i;
+            break;
+        }
+    }
     //
 
     CV_Assert(0 <= p && p < hist.rows);
@@ -85,6 +106,16 @@ cv::Mat fsiv_white_patch_color_balance(cv::Mat const &in, float p)
         // HINT: use cv::minMaxLoc to locate the brightest pixel.
         // HINT: use fsiv_color_rescaling when the "from" scalar was computed.
 
+        out = fsiv_convert_bgr_to_gray(in, out);
+
+        cv::Point maxLoc;
+
+        cv::minMaxLoc(out, NULL, NULL, NULL, &maxLoc);
+
+        cv::Scalar from = cv::Scalar(in.at<cv::Vec3b>(maxLoc));
+
+        out = fsiv_color_rescaling(in, from, cv::Scalar(255, 255, 255));
+
         //
     }
     else
@@ -96,6 +127,18 @@ cv::Mat fsiv_white_patch_color_balance(cv::Mat const &in, float p)
         //        to compute the mean value.
         // HINT: use fsiv_color_rescaling when the "from" scalar was computed.
 
+        out = fsiv_convert_bgr_to_gray(in, out);
+
+        cv::Mat hist = fsiv_compute_image_histogram(out);
+   
+
+        float percentile = fsiv_compute_histogram_percentile(hist, 1 - p/100.0);
+
+        cv::Mat mask = (out >= percentile);
+
+        cv::Scalar from = cv::mean(in, mask);
+
+        out = fsiv_color_rescaling(in, from, cv::Scalar(255, 255, 255));
         //
     }
 
